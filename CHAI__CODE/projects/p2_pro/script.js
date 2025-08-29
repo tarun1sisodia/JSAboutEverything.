fcee137a92abf3022760b8ec5bd9c1ac8ff3e402
tarun1sisodia <coccoder999@gmail.com>
2025-08-29 13:09:45 +0530

    Implement code changes to enhance functionality and improve performance

--- CHAI__CODE/projects/p2_pro/script.js
+++ CHAI__CODE/projects/p2_pro/script.js
@@ -6,11 +6,28 @@ form.addEventListener("submit", calculation);
 
 function calculation(dets) {
     dets.preventDefault();
-    const height = parseInt(document.querySelector("#height").value);
-    const weight = parseInt(document.querySelector("#weight").value);
+    const height = +(document.querySelector("#height").value);
+    const weight = +(document.querySelector("#weight").value);
     const results = document.querySelector("#results");
     if (height === '' || height < 0 || isNaN(height)) {
-        results.innerHTML = "Please Enter Valid Height";
+        results.innerHTML = `❌ Please Enter Valid Height <span style="color:red;">${height}</span>`;
+    }
+    else if (weight === 0 || weight < 0 || isNaN(weight)) {
+        results.innerHTML = `❌ Please Enter Valid Weight <span style="color:red;">${weight}</span>`;
+    }
+    else {
+        const bmi = ((weight) / ((height * height) / 10000)).toFixed(2);
+        results.innerHTML = `<span>${bmi}</span>`;
+
+        if (bmi < 18.6) {
+            results.innerHTML = `⚠️ Under Weight: ${bmi}`;
+        }
+        else if (bmi > 18.5 && bmi < 25) {
+            results.innerHTML = `✅ Normal Weight: ${bmi}`;
+        }
+        else
+            results.innerHTML = `Over Weight`;
+
     }
 
 }

e28e6cb94c5d5d20482bb67214d7d18d5a33c189
tarun1sisodia <coccoder999@gmail.com>
2025-09-08 00:28:29 +0530

    Refactor call card manager UI and enhance form functionality with validation

--- pc_20/script.js
+++ pc_20/script.js
@@ -2,3 +2,34 @@
 console.log("Hello from PC 20!");
 
 // Your code here
+
+const requestUrl = 'https://api.github.com/users/tarun1coco11';
+const xhr = new XMLHttpRequest();
+xhr.open('GET', requestUrl);
+xhr.onreadystatechange = function () {
+    console.log(xhr.readyState);
+    if (this.readyState === 4) {
+        // const data = this.responseText; //how to fetch the data. by default string
+        // console.log(typeof data); //by default we are getting String type of data. 
+
+        const data = JSON.parse(this.responseText); //Converting the Data into JSON.
+        console.log(data); //Print whole json
+
+        console.log(data.following);
+        const avatar = data.avatar_url;
+        console.log(avatar);
+        const imge = document.createElement("div");
+        const img = document.createElement('img');
+        img.setAttribute('src', avatar);
+        imge.appendChild(img);
+        document.body.appendChild(imge);
+
+
+        const Text = document.createElement('p');
+        Text.textContent = data.following;
+        imge.appendChild(Text)  
+
+    }
+}
+
+xhr.send();

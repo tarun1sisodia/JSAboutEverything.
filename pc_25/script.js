5936065aaac4661d1644fd9c17c65b7a27bc98de
tarun1sisodia <coccoder999@gmail.com>
2025-09-24 18:15:54 +0530

    learned about more new and use of it.

--- pc_25/script.js
+++ pc_25/script.js
@@ -2,3 +2,18 @@
 console.log("Hello from PC 25!");
 
 // Your code here
+function Setusername(username) {
+    this.username = username; // this will be gayab after executing.. that why we all should use 
+    console.log(`Rn it is Running:`);
+
+}
+function CreateUser(username, email, password) {
+    Setusername.call(this, username);
+    // Setusername(username); // Rn it will not run cuz we did not call it properly.
+    this.email = email
+    this.password = password
+}
+
+const chai = new CreateUser("Tarun", "Aisodia", '1231');
+console.log(chai);
+

4661bc8b52fb13e458c472fb5c2fcf424adda5c1
tarun1sisodia <coccoder999@gmail.com>
2025-09-26 23:43:28 +0530

    Refactor User class implementation and enhance functionality with password encryption

--- pc_26/script.js
+++ pc_26/script.js
@@ -1,4 +1,45 @@
-// PC 26 JavaScript file
+/* // PC 26 JavaScript file
 console.log("Hello from PC 26!");
 
 // Your code here
+class User {
+    constructor(username, email, password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    encryptPassword() {
+        return `${this.password}abc`;
+    }
+
+    changeUserName() {
+        return `${this.username.toLowerCase()}`
+    }
+
+}
+
+// if we want to know what the encrypted Password:
+
+console.log(chai.encryptPassword());
+console.log(chai.changeUserName());
+*/
+
+//Behind the scenes;
+
+function User(username, email, password) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+}
+
+User.prototype.changeUserName = function () {
+    return `${this.username.toLowerCase();}Its Safe`;
+}
+const chai = new User("CHai", "chai@gmail.com", "1239128");
+console.log(chai);
+
+// if we want to know what the encrypted Password:
+
+// console.log(chai.encryptPassword());
+console.log(chai.changeUserName);
